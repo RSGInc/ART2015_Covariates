6494b9f3cd58e29b4658baf953d174572328f5f3
unknown <jeff.keller@10162W.i-rsg.com>
2015-05-28 18:04:58 -0400

    Initial commit of modeling files.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ART2015_Covariates
 Data and code for reproducing the analysis in the 2015 ART Forum Poster "Understanding How Covariates Perform Across Different HB Packages".
+
+Analysis performed with:
+
+- Sawtooth CBC HB v5.5.3
+- R v3.2.0
+- data.table R package v1.9.4
+- RSGHB R package v1.1.0
---
+++ RSGHB/1_estimation.R
@@ -0,0 +1,268 @@
+###################################
+# Setup and Data Preparation
+
+# Set the working directory and load necessary packages
+setwd("C:/Users/jeff.keller/Desktop/ART2015_Covariates/RSGHB/")
+library(RSGHB)
+library(data.table)
+
+# Fetch the choice data
+load("choicedata.RData")
+
+# Remove hold-out sample
+choicedata <- cd[out_hold == 0 & sequence!=3]
+
+###################################
+# Defining the Likelihood Functions
+
+# Without Covariates
+Likelihoods_NCV <- function(fc, b) {  
+  
+  # Define Parameters
+  cc <- 1
+  beta_pos.left <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att1.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att1.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att1.Level3 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att1.Level4 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att1.Level5 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att2.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att2.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att2.Level3 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att2.Level4 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att3.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att3.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att3.Level3 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att3.Level4 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att4.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att4.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att4.Level3 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att5.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att5.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att5.Level3 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att6.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att6.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att6.Level3 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att7.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att7.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att8.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att8.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  
+  # Calculate Utility
+  choicedata[, U :=  beta_pos.left * (pos == "left") +
+               beta_att1.Level1 * (att1 == "Level1") +
+               beta_att1.Level2 * (att1 == "Level2") +
+               beta_att1.Level3 * (att1 == "Level3") +
+               beta_att1.Level4 * (att1 == "Level4") +
+               beta_att1.Level5 * (att1 == "Level5") +
+               beta_att2.Level1 * (att2 == "Level1") +
+               beta_att2.Level2 * (att2 == "Level2") +
+               beta_att2.Level3 * (att2 == "Level3") +
+               beta_att2.Level4 * (att2 == "Level4") +
+               beta_att3.Level1 * (att3 == "Level1") +
+               beta_att3.Level2 * (att3 == "Level2") +
+               beta_att3.Level3 * (att3 == "Level3") +
+               beta_att3.Level4 * (att3 == "Level4") +
+               beta_att4.Level1 * (att4 == "Level1") +
+               beta_att4.Level2 * (att4 == "Level2") +
+               beta_att4.Level3 * (att4 == "Level3") +
+               beta_att5.Level1 * (att5 == "Level1") +
+               beta_att5.Level2 * (att5 == "Level2") +
+               beta_att5.Level3 * (att5 == "Level3") +
+               beta_att6.Level1 * (att6 == "Level1") +
+               beta_att6.Level2 * (att6 == "Level2") +
+               beta_att6.Level3 * (att6 == "Level3") +
+               beta_att7.Level1 * (att7 == "Level1") +
+               beta_att7.Level2 * (att7 == "Level2") +
+               beta_att8.Level1 * (att8 == "Level1") +
+               beta_att8.Level2 * (att8 == "Level2")]
+  
+  # Calculate Probabilities
+  choicedata[, P := exp(U)/sum(exp(U)), by = list(parent_id, sequence)]
+  return(choicedata[choice == 1, P])
+  
+}
+
+# With Covariates
+Likelihoods_CV <- function(fc, b) {  
+ 
+  # Define Fixed Parameters
+  cc <- 1
+  cov1_pos.left <- fc[cc]; cc <- cc + 1
+  cov2_pos.left <- fc[cc]; cc <- cc + 1
+  cov1_att1.Level1 <- fc[cc]; cc <- cc + 1
+  cov2_att1.Level1 <- fc[cc]; cc <- cc + 1
+  cov1_att1.Level2 <- fc[cc]; cc <- cc + 1
+  cov2_att1.Level2 <- fc[cc]; cc <- cc + 1
+  cov1_att1.Level3 <- fc[cc]; cc <- cc + 1
+  cov2_att1.Level3 <- fc[cc]; cc <- cc + 1
+  cov1_att1.Level4 <- fc[cc]; cc <- cc + 1
+  cov2_att1.Level4 <- fc[cc]; cc <- cc + 1
+  cov1_att1.Level5 <- fc[cc]; cc <- cc + 1
+  cov2_att1.Level5 <- fc[cc]; cc <- cc + 1
+  cov1_att2.Level1 <- fc[cc]; cc <- cc + 1
+  cov2_att2.Level1 <- fc[cc]; cc <- cc + 1
+  cov1_att2.Level2 <- fc[cc]; cc <- cc + 1
+  cov2_att2.Level2 <- fc[cc]; cc <- cc + 1
+  cov1_att2.Level3 <- fc[cc]; cc <- cc + 1
+  cov2_att2.Level3 <- fc[cc]; cc <- cc + 1 
+  cov1_att2.Level4 <- fc[cc]; cc <- cc + 1
+  cov2_att2.Level4 <- fc[cc]; cc <- cc + 1
+  cov1_att3.Level1 <- fc[cc]; cc <- cc + 1
+  cov2_att3.Level1 <- fc[cc]; cc <- cc + 1
+  cov1_att3.Level2 <- fc[cc]; cc <- cc + 1
+  cov2_att3.Level2 <- fc[cc]; cc <- cc + 1
+  cov1_att3.Level3 <- fc[cc]; cc <- cc + 1
+  cov2_att3.Level3 <- fc[cc]; cc <- cc + 1
+  cov1_att3.Level4 <- fc[cc]; cc <- cc + 1
+  cov2_att3.Level4 <- fc[cc]; cc <- cc + 1
+  cov1_att4.Level1 <- fc[cc]; cc <- cc + 1
+  cov2_att4.Level1 <- fc[cc]; cc <- cc + 1
+  cov1_att4.Level2 <- fc[cc]; cc <- cc + 1
+  cov2_att4.Level2 <- fc[cc]; cc <- cc + 1
+  cov1_att4.Level3 <- fc[cc]; cc <- cc + 1
+  cov2_att4.Level3 <- fc[cc]; cc <- cc + 1
+  cov1_att5.Level1 <- fc[cc]; cc <- cc + 1
+  cov2_att5.Level1 <- fc[cc]; cc <- cc + 1
+  cov1_att5.Level2 <- fc[cc]; cc <- cc + 1
+  cov2_att5.Level2 <- fc[cc]; cc <- cc + 1
+  cov1_att5.Level3 <- fc[cc]; cc <- cc + 1
+  cov2_att5.Level3 <- fc[cc]; cc <- cc + 1 
+  cov1_att6.Level1 <- fc[cc]; cc <- cc + 1
+  cov2_att6.Level1 <- fc[cc]; cc <- cc + 1
+  cov1_att6.Level2 <- fc[cc]; cc <- cc + 1
+  cov2_att6.Level2 <- fc[cc]; cc <- cc + 1
+  cov1_att6.Level3 <- fc[cc]; cc <- cc + 1
+  cov2_att6.Level3 <- fc[cc]; cc <- cc + 1
+  cov1_att7.Level1 <- fc[cc]; cc <- cc + 1
+  cov2_att7.Level1 <- fc[cc]; cc <- cc + 1
+  cov1_att7.Level2 <- fc[cc]; cc <- cc + 1
+  cov2_att7.Level2 <- fc[cc]; cc <- cc + 1
+  cov1_att8.Level1 <- fc[cc]; cc <- cc + 1
+  cov2_att8.Level1 <- fc[cc]; cc <- cc + 1
+  cov1_att8.Level2 <- fc[cc]; cc <- cc + 1
+  cov2_att8.Level2 <- fc[cc]; cc <- cc + 1
+  
+  # Define Random Parameters
+  cc <- 1
+  beta_pos.left    <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att1.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att1.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att1.Level3 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att1.Level4 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att1.Level5 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att2.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att2.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att2.Level3 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att2.Level4 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att3.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att3.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att3.Level3 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att3.Level4 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att4.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att4.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att4.Level3 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att5.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att5.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att5.Level3 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att6.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att6.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att6.Level3 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att7.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att7.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att8.Level1 <- rep(b[,cc], each = 2); cc <- cc + 1
+  beta_att8.Level2 <- rep(b[,cc], each = 2); cc <- cc + 1
+  
+  # Calculate Utility
+  choicedata[, U :=  (beta_pos.left + cov1_pos.left * (cov == "cov1") + cov2_pos.left * (cov == "cov2")) * (pos == "left") +
+                     (beta_att1.Level1 + cov1_att1.Level1 * (cov == "cov1") + cov2_att1.Level1 * (cov == "cov2")) * (att1 == "Level1") +
+                     (beta_att1.Level2 + cov1_att1.Level2 * (cov == "cov1") + cov2_att1.Level2 * (cov == "cov2")) * (att1 == "Level2") +
+                     (beta_att1.Level3 + cov1_att1.Level3 * (cov == "cov1") + cov2_att1.Level3 * (cov == "cov2")) * (att1 == "Level3") +
+                     (beta_att1.Level4 + cov1_att1.Level4 * (cov == "cov1") + cov2_att1.Level4 * (cov == "cov2")) * (att1 == "Level4") +
+                     (beta_att1.Level5 + cov1_att1.Level5 * (cov == "cov1") + cov2_att1.Level5 * (cov == "cov2")) * (att1 == "Level5") +
+                     (beta_att2.Level1 + cov1_att2.Level1 * (cov == "cov1") + cov2_att2.Level1 * (cov == "cov2")) * (att2 == "Level1") +
+                     (beta_att2.Level2 + cov1_att2.Level2 * (cov == "cov1") + cov2_att2.Level2 * (cov == "cov2")) * (att2 == "Level2") +
+                     (beta_att2.Level3 + cov1_att2.Level3 * (cov == "cov1") + cov2_att2.Level3 * (cov == "cov2")) * (att2 == "Level3") +
+                     (beta_att2.Level4 + cov1_att2.Level4 * (cov == "cov1") + cov2_att2.Level4 * (cov == "cov2")) * (att2 == "Level4") +
+                     (beta_att3.Level1 + cov1_att3.Level1 * (cov == "cov1") + cov2_att3.Level1 * (cov == "cov2")) * (att3 == "Level1") +
+                     (beta_att3.Level2 + cov1_att3.Level2 * (cov == "cov1") + cov2_att3.Level2 * (cov == "cov2")) * (att3 == "Level2") +
+                     (beta_att3.Level3 + cov1_att3.Level3 * (cov == "cov1") + cov2_att3.Level3 * (cov == "cov2")) * (att3 == "Level3") +
+                     (beta_att3.Level4 + cov1_att3.Level4 * (cov == "cov1") + cov2_att3.Level4 * (cov == "cov2")) * (att3 == "Level4") +
+                     (beta_att4.Level1 + cov1_att4.Level1 * (cov == "cov1") + cov2_att4.Level1 * (cov == "cov2")) * (att4 == "Level1") +
+                     (beta_att4.Level2 + cov1_att4.Level2 * (cov == "cov1") + cov2_att4.Level2 * (cov == "cov2")) * (att4 == "Level2") +
+                     (beta_att4.Level3 + cov1_att4.Level3 * (cov == "cov1") + cov2_att4.Level3 * (cov == "cov2")) * (att4 == "Level3") +
+                     (beta_att5.Level1 + cov1_att5.Level1 * (cov == "cov1") + cov2_att5.Level1 * (cov == "cov2")) * (att5 == "Level1") +
+                     (beta_att5.Level2 + cov1_att5.Level2 * (cov == "cov1") + cov2_att5.Level2 * (cov == "cov2")) * (att5 == "Level2") +
+                     (beta_att5.Level3 + cov1_att5.Level3 * (cov == "cov1") + cov2_att5.Level3 * (cov == "cov2")) * (att5 == "Level3") +
+                     (beta_att6.Level1 + cov1_att6.Level1 * (cov == "cov1") + cov2_att6.Level1 * (cov == "cov2")) * (att6 == "Level1") +
+                     (beta_att6.Level2 + cov1_att6.Level2 * (cov == "cov1") + cov2_att6.Level2 * (cov == "cov2")) * (att6 == "Level2") +
+                     (beta_att6.Level3 + cov1_att6.Level3 * (cov == "cov1") + cov2_att6.Level3 * (cov == "cov2")) * (att6 == "Level3") +
+                     (beta_att7.Level1 + cov1_att7.Level1 * (cov == "cov1") + cov2_att7.Level1 * (cov == "cov2")) * (att7 == "Level1") +
+                     (beta_att7.Level2 + cov1_att7.Level2 * (cov == "cov1") + cov2_att7.Level2 * (cov == "cov2")) * (att7 == "Level2") +
+                     (beta_att8.Level1 + cov1_att8.Level1 * (cov == "cov1") + cov2_att8.Level1 * (cov == "cov2")) * (att8 == "Level1") +
+                     (beta_att8.Level2 + cov1_att8.Level2 * (cov == "cov1") + cov2_att8.Level2 * (cov == "cov2")) * (att8 == "Level2")]
+  
+  # Calculate Probabilities
+  choicedata[, P := exp(U)/sum(exp(U)), by = list(parent_id, sequence)]
+  return(choicedata[choice == 1, P])
+  
+}
+
+###################################
+### Model Controls and Settings
+
+# Random parameter names
+params <- c("pos.left", "att1.Level1", "att1.Level2", "att1.Level3", "att1.Level4", 
+            "att1.Level5", "att2.Level1", "att2.Level2", "att2.Level3", "att2.Level4",
+            "att3.Level1", "att3.Level2", "att3.Level3", "att3.Level4", "att4.Level1",
+            "att4.Level2", "att4.Level3", "att5.Level1", "att5.Level2", "att5.Level3",
+            "att6.Level1", "att6.Level2", "att6.Level3", "att7.Level1", "att7.Level2",
+            "att8.Level1", "att8.Level2")
+
+# Covariate parameter names
+covariates <- c("cov1_pos.left", "cov2_pos.left", "cov1_att1.Level1", "cov2_att1.Level1", "cov1_att1.Level2", "cov2_att1.Level2",
+                "cov1_att1.Level3", "cov2_att1.Level3", "cov1_att1.Level4", "cov2_att1.Level4", "cov1_att1.Level5", "cov2_att1.Level5", "cov1_att2.Level1",
+                "cov2_att2.Level1", "cov1_att2.Level2", "cov2_att2.Level2", "cov1_att2.Level3", "cov2_att2.Level3", "cov1_att2.Level4", "cov2_att2.Level4",
+                "cov1_att3.Level1", "cov2_att3.Level1", "cov1_att3.Level2", "cov2_att3.Level2", "cov1_att3.Level3", "cov2_att3.Level3", "cov1_att3.Level4", "cov2_att3.Level4",
+                "cov1_att4.Level1", "cov2_att4.Level1", "cov1_att4.Level2", "cov2_att4.Level2", "cov1_att4.Level3", "cov2_att4.Level3", "cov1_att5.Level1", "cov2_att5.Level1",
+                "cov1_att5.Level2", "cov2_att5.Level2", "cov1_att5.Level3", "cov2_att5.Level3", "cov1_att6.Level1", "cov2_att6.Level1", "cov1_att6.Level2", "cov2_att6.Level2", "cov1_att6.Level3", "cov2_att6.Level3",
+                "cov1_att7.Level1", "cov2_att7.Level1", "cov1_att7.Level2", "cov2_att7.Level2", "cov1_att8.Level1", "cov2_att8.Level1", "cov1_att8.Level2", "cov2_att8.Level2")
+
+# ID variable represents the individuals associated with the output of the Likelihood functions
+ID <- data.frame(unique(choicedata[, list(parent_id, sequence)]))
+names(ID) <- c("ID", "sequence")
+
+##############################
+### Estimate Models
+
+# Without Covariates (Default Settings) 4128 seconds
+system.time(NCV_def <- doHB(Likelihoods_NCV, choicedata = ID,
+                            control = list(gVarNamesFixed = NULL, gVarNamesNormal = params,
+                                           modelname = "NCV_def", nodiagnostics = TRUE,
+                                           gNCREP = 100000, gNEREP = 10000, gSeed = 1991)))
+
+# Without Covariates (Modified Settings)
+system.time(NCV_mod <- doHB(Likelihoods_NCV, choicedata = ID,
+                            control = list(gVarNamesFixed = NULL, gVarNamesNormal = params,
+                                           modelname = "NCV_mod", nodiagnostics = TRUE,
+                                           gNCREP = 100000, gNEREP = 10000, gSeed = 1991,
+                                           degreesOfFreedom = 0, priorVariance = 27)))
+
+# With Covariates (Default Settings)
+system.time(CV_def <- doHB(Likelihoods_CV, choicedata = ID,
+                           control = list(gVarNamesFixed = covariates, gVarNamesNormal = params,
+                                          modelname = "CV_def", nodiagnostics = TRUE,
+                                          gNCREP = 100000, gNEREP = 10000, gSeed = 1991)))
+
+# With Covariates (Modified Settings)
+system.time(CV_mod <- doHB(Likelihoods_CV, choicedata = ID,
+                           control = list(gVarNamesFixed = covariates, gVarNamesNormal = params,
+                                          modelname = "CV_mod", nodiagnostics = TRUE,
+                                          gNCREP = 100000, gNEREP = 10000, gSeed = 1991,
+                                          degreesOfFreedom = 0, priorVariance = 27)))
+
+# Save Models
+save(NCV_def, file = "NCV_def.RData")
+save(NCV_mod, file = "NCV_mod.RData")
+save(CV_def,  file = "CV_def.RData")
+save(CV_mod,  file = "CV_mod.RData")
